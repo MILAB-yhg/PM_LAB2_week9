9f89cb9ca058374abd573cd26e7fbf4a228ca17e
sjw7323 <39179960+sjw7323@users.noreply.github.com>
2018-05-11 05:12:22 +0000

    Update main.cpp

--- Lab2_9_HW/Lab2_9_HW/main.cpp
+++ Lab2_9_HW/Lab2_9_HW/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void main() {	
 	cout << "hello word!" << endl;	
-	cout << "hello c++!" << endl;			
+	cout << "2017-10194 " << endl;			
 	/* TODO:
 		print your name & student ID 	
 	*/
