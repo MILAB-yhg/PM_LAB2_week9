60422ce4c95462747066ba5af691bf8eeac7cea7
Jee sung Kim <kim024680@naver.com>
2018-05-11 05:05:09 +0000

    Update main.cpp

--- Lab2_9_HW/Lab2_9_HW/main.cpp
+++ Lab2_9_HW/Lab2_9_HW/main.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 void main() {
-	cout << "hello word!" << endl;			/* TODO:
+	cout << "hello word!" << endl;			
+	/* TODO:
 		print your name & student ID 	
 	*/
+	cout << "2017-13939 Jee Sung Kim" << endl;
 }
