c95d8076eeb53b8b984646f7eeb66da6deeabbb9
이수형 <soohyung99@naver.com>
2018-05-15 22:14:53 +0900

    2017 - 18571

--- Lab2_9_HW/Lab2_9_HW/main.cpp
+++ Lab2_9_HW/Lab2_9_HW/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void main() {
 	cout << "hello word!" << endl;
-	cout << "ÀÌ¼öÇü Soohyung Lee" << endl;
+	cout << "Soohyung Lee" << endl;
 	cout << "2017-18571" << endl;
 	/* TODO:
 		print your name & student ID 	
